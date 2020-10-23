5ef181a883d4de1ba0d972ce0100aebc2a66bc32
phundq <nguyenduyquocphu1998@gmail.com>
2020-10-23 17:04:52 +0700

    [docs] update chat.getway

--- src/chat/chat.getway.ts
+++ src/chat/chat.getway.ts
@@ -4,24 +4,24 @@ import { Messenger } from './message.i';
 
 @WebSocketGateway()
 export class ChatGetway {
-     @WebSocketServer()
+  @WebSocketServer()
   server: Server;
   @SubscribeMessage('chatToServer')
-  handChat(client : Socket ,payload :Messenger){
+  handChat(client: Socket, payload: Messenger) {
     console.log(payload);
-    try { 
-			this.server.in(payload.room).clients((error, clients) => {
-				if (error) throw error;
-				console.log(clients); 
-			});
-		} catch(err) {
-			console.log(err);
-		}
-    this.server.to(payload.room).emit('chatToClient',payload);
+    try {
+      this.server.in(payload.room).clients((error, clients) => {
+        if (error) throw error;
+        console.log(clients);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    this.server.to(payload.room).emit('chatToClient', payload);
   }
 
   @SubscribeMessage('joinRoom')
-  joinRoom(client : Socket ,room :string){
+  joinRoom(client: Socket, room: string) {
     client.join(room);
   }
 }
